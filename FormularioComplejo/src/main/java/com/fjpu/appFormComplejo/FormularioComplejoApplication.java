6f13eecbc02507645a45944ddf4781b086bc13b7
fprezur <fperezur@gmail.com>
2024-04-11 18:49:12 +0200

    Proyecto FormularioComplejo y detallesproyectos

---
+++ FormularioComplejo/src/main/java/com/fjpu/appFormComplejo/FormularioComplejoApplication.java
@@ -0,0 +1,13 @@
+package com.fjpu.appFormComplejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularioComplejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularioComplejoApplication.class, args);
+	}
+
+}
---
+++ FormularioComplejo/src/test/java/com/fjpu/appFormComplejo/FormularioComplejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fjpu.appFormComplejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularioComplejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
